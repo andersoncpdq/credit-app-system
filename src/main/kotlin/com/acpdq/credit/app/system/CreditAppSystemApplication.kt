80324ec14eeb0456806f3d30e9d147860884f273
Anderson Couto <andersoncpdq@gmail.com>
2023-10-29 18:09:10 -0300

    projeto criado

---
+++ src/main/kotlin/com/acpdq/credit/app/system/CreditAppSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.acpdq.credit.app.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditAppSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditAppSystemApplication>(*args)
+}
