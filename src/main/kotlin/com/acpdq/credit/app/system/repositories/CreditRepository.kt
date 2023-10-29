73932b69328f7ab7c26728cd7783f3113f2c6fc1
Anderson Couto <andersoncpdq@gmail.com>
2023-10-29 20:14:08 -0300

    consultas findByCreditCode e findAllByCustomerId

--- src/main/kotlin/com/acpdq/credit/app/system/repositories/CreditRepository.kt
+++ src/main/kotlin/com/acpdq/credit/app/system/repositories/CreditRepository.kt
@@ -2,8 +2,14 @@ package com.acpdq.credit.app.system.repositories
 
 import com.acpdq.credit.app.system.entities.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface CreditRepository: JpaRepository<Credit, Long> {
+    fun findByCreditCode(creditCode: UUID): Credit
+
+    @Query("SELECT * FROM tb_credit WHERE customer_id = ?1", nativeQuery = true)
+    fun findAllByCustomerId(customerId: Long): List<Credit>
 }
